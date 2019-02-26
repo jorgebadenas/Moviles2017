518bf32edb291de12530fb17f3cf7a38c834694b
Jorge Badenas <badenas@uji.es>
2019-02-26 14:26:40 +0100

    Cambios del día 26-02-2019 2

--- src/main/java/aplicacion/Aplicacion.java
+++ src/main/java/aplicacion/Aplicacion.java
@@ -28,11 +28,20 @@ public class Aplicacion {
         int opcion;
         Scanner sc = new Scanner(System.in);
         do {
+            mostrarOpcionesMenuPrincipal();
             opcion = sc.nextInt();
             if(opcion<0 || opcion>3)
                 System.out.println("Opción incorrecta. Por favor, vuelva a escoger opción.");
         } while(opcion<0 || opcion>3);
         return opcion;
+    }
+
+    private static void mostrarOpcionesMenuPrincipal() {
+        System.out.println("0. Salir.");
+        System.out.println("1. Clientes.");
+        System.out.println("2. Llamadas.");
+        System.out.println("3. Facturas.");
+        System.out.println("Seleccione una opción.");
     }
 
     private static void menuFacturas() {
